4afba5dd7e58dd7a4bfd78435db677e6538e4b30
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-11 20:34:23 +0530

    Adding code for Practice Question 2 for this lecture

---
+++ Variables_and_Data_Types/PracticeQuestion2.js
@@ -0,0 +1,21 @@
+const profile = {
+    userName : "shradhakhapra",
+    fullName : "Shradha Khapra",
+    posts : 195,
+    followers : 569000,
+    following : 4,
+    threadsUserName : "shradhakhapra",
+    category: "Entrepreneur",
+    bio: "Apna College | Ex-Microsoft, DRDO\nTo educate someone is the highest privilege❤️",
+    isFollow: false
+}
+
+console.log("Username: " + profile.userName);
+console.log("Full Name: " + profile.fullName);
+console.log("Am I following this profile: " + profile.isFollow);
+console.log("Posts: " + profile.posts);
+console.log("Followers: " + profile.followers);
+console.log("Following: " + profile.following);
+console.log("Threads Username: " + profile.threadsUserName);
+console.log("Category: " + profile.category);
+console.log("Bio:\n" + profile.bio);
