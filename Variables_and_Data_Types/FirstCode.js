fb6a856d512c82d06b45e98e3913f8cdbf23800b
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-11 18:04:36 +0530

    Commenting unnecessary console.log statements

--- Variables_and_Data_Types/FirstCode.js
+++ Variables_and_Data_Types/FirstCode.js
@@ -5,27 +5,27 @@ console.log("I hope you love javacript");
 
 // understanding variables and variables naming rules and conventions
 fullName = "Jesus Christ";
-console.log(fullName);
+// console.log(fullName);
 age = 119;
-console.log(age);
+// console.log(age);
 price = 99.91;
-console.log(price);
+// console.log(price);
 x = null;
-console.log(x);
+// console.log(x);
 y = undefined;
-console.log(y);
+// console.log(y);
 radius = 8721;
 isFollow = false;
-console.log(isFollow);
+// console.log(isFollow);
 isFollow = true;
-console.log(isFollow);
+// console.log(isFollow);
 fullName = 32453;
-console.log(fullName);
+// console.log(fullName);
 fullname = "Sarthak Giri";
 FULLNAME = "Javscript Coding";
-console.log(fullname);
-console.log(FULLNAME);
-console.log(fullName);
+// console.log(fullname);
+// console.log(FULLNAME);
+// console.log(fullName);
 Console = "Using reserved keywords";
 console.log(Console);
 
