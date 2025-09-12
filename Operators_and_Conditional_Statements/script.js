0c3da6ff8d47d59306dd776fec019f7a1e0e3268
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-12 22:09:12 +0530

    Adding code for understanding comment syntax types in javascript

---
+++ Operators_and_Conditional_Statements/script.js
@@ -0,0 +1,7 @@
+// Comments in JS
+console.log("hello world!"); // code to print "hello world!" in console
+
+console.log("Welcome to javascript");
+/* This is a code that print "Welcome to javascript"
+in the console window */
+
