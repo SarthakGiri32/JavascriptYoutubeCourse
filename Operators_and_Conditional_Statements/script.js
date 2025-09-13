acc6422e8e01f6c70d70b4739c3e0fa30ad234c6
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-13 20:10:08 +0530

    Adding code for understanding comparison operators

--- Operators_and_Conditional_Statements/script.js
+++ Operators_and_Conditional_Statements/script.js
@@ -8,10 +8,10 @@ in the console window */
 // II) Operators in JS
 // 1. Arithmetic Operators
 // a) operators that need two operands
-let a = 23436565;
-let b = 8767684;
+// let a = 23436565;
+// let b = 8767684;
 // let c = a + b;
-let x = 5, y = 3;
+// let x = 5, y = 3;
 // console.log("a =", a, "& b =", b);
 // console.log("x =", x, "& y =", y);
 // console.log("a + b =", c);
@@ -22,16 +22,16 @@ let x = 5, y = 3;
 // console.log("x ** y =", x ** y);
 
 // b) operators that need one operand (Unary operators)
-a++;
+// a++;
 // console.log("a++ =", a);
-a--;
+// a--;
 // console.log("a-- =", a);
 // console.log("++a =", ++a, "; a =", a); // pre-increment
 // console.log("a++ =", a++, "; a =", a); // post-increment
 // console.log("--a =", --a, "; a =", a); // pre-decrement
 // console.log("a-- =", a--, "; a =", a); // post-decrement
 
-// 2) Assignment Operators
+// 2. Assignment Operators
 // a += 977671;
 // console.log("a =", a);
 // a -= 5435335;
@@ -44,3 +44,24 @@ a--;
 // console.log("x =", x);
 // y **= 5;
 // console.log("y =", y);
+
+// 3. Comparison Operators
+let a = 23436565;
+let b = 8767684;
+let x = 5, y = 3;
+
+console.log("a == b :", a == b);
+console.log("a != b :", a != b);
+let num1 = 52342;
+let num2 = "52342";
+console.log("num1 == num2 :", num1 == num2);
+console.log("num1 != num2 :", num1 != num2);
+console.log("num1 === num2 :", num1 === num2);
+console.log("num1 !== num2 :", num1 !== num2);
+
+console.log("x > y :", x > y);
+console.log("x < y :", x < y);
+num1 = 456454;
+num2 = 9887;
+console.log("num1 <= num2 :", num1 <= num2);
+console.log("num1 >= num2 :", num1 >= num2);
