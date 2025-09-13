aaf9992ff40d69e958343c69aa0521850de9d7b3
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-13 19:35:24 +0530

    Adding code for understanding assignment operators

--- Operators_and_Conditional_Statements/script.js
+++ Operators_and_Conditional_Statements/script.js
@@ -1,19 +1,19 @@
-// Comments in JS
+// I) Comments in JS
 // console.log("hello world!"); // code to print "hello world!" in console
 
 // console.log("Welcome to javascript");
 /* This is a code that print "Welcome to javascript"
 in the console window */
 
-// Operators in JS
+// II) Operators in JS
 // 1. Arithmetic Operators
 // a) operators that need two operands
 let a = 23436565;
 let b = 8767684;
 // let c = a + b;
 let x = 5, y = 3;
-console.log("a =", a, "& b =", b);
-console.log("x =", x, "& y =", y);
+// console.log("a =", a, "& b =", b);
+// console.log("x =", x, "& y =", y);
 // console.log("a + b =", c);
 // console.log("a - b =", a - b);
 // console.log("a * b =", a * b);
@@ -23,10 +23,24 @@ console.log("x =", x, "& y =", y);
 
 // b) operators that need one operand (Unary operators)
 a++;
-console.log("a++ =", a);
+// console.log("a++ =", a);
 a--;
-console.log("a-- =", a);
-console.log("++a =", ++a, "; a =", a); // pre-increment
-console.log("a++ =", a++, "; a =", a); // post-increment
-console.log("--a =", --a, "; a =", a); // pre-decrement
-console.log("a-- =", a--, "; a =", a); // post-decrement
+// console.log("a-- =", a);
+// console.log("++a =", ++a, "; a =", a); // pre-increment
+// console.log("a++ =", a++, "; a =", a); // post-increment
+// console.log("--a =", --a, "; a =", a); // pre-decrement
+// console.log("a-- =", a--, "; a =", a); // post-decrement
+
+// 2) Assignment Operators
+// a += 977671;
+// console.log("a =", a);
+// a -= 5435335;
+// console.log("a =", a);
+// x *= 34;
+// console.log("x =", x);
+// a /= 34211;
+// console.log("a =", a);
+// x %= 19;
+// console.log("x =", x);
+// y **= 5;
+// console.log("y =", y);
