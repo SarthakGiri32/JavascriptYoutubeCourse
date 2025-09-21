95fc39438cf92fc75d550870cd36e952c962fda2
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-21 19:22:15 +0530

    Beginning Strings tutorial and renaming "LoopsAndStrings.js"

---
+++ Loops_and_Strings/Strings/Strings.js
@@ -0,0 +1,5 @@
+// String length
+let strVar = "Welcome to Javascript strings!";
+let strVarLength = strVar.length;
+console.log("strVar :", strVar);
+console.log("strVar.length = " + strVarLength);
