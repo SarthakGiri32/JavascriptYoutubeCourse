e4081c5e5d155b9e517fc4bd0c2cf4443f8e8f45
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-21 20:35:44 +0530

    Adding code for understanding template literals

--- Loops_and_Strings/Strings/Strings.js
+++ Loops_and_Strings/Strings/Strings.js
@@ -1,11 +1,20 @@
-// String length
-let strVar = "Welcome to Javascript strings!";
-let strVarLength = strVar.length;
-let strVarLength2 = strVar["length"]; // using square brackets to access string length property
-console.log("strVar :", strVar);
-console.log("strVar.length = " + strVarLength + ' or strVar["length"] = ' + strVarLength2);
+// I) Intro to strings
+// String length property
+// let strVar = "Welcome to Javascript strings!";
+// let strVarLength = strVar.length;
+// let strVarLength2 = strVar["length"]; // using square brackets to access string length property
+// console.log("strVar :", strVar);
+// console.log("strVar.length = " + strVarLength + ' or strVar["length"] = ' + strVarLength2);
 
 // accessing individual characters
-console.log("strVar[9] : " + strVar[9]);
-let charPos = 22;
-console.log("strVar[" + charPos + "] : " +strVar[charPos]); // cannot use dot notation; prints "undefined"
+// console.log("strVar[9] : " + strVar[9]);
+// let charPos = 22;
+// console.log("strVar[" + charPos + "] : " + strVar[charPos]); // cannot use dot notation; prints "undefined"
+
+// II) Template Literal
+const exampleObject = {
+    item: "Gel Pen",
+    price: 30
+};
+
+console.log(`The cost of a ${exampleObject.item} = ₹${exampleObject.price}`); // string interpolation
