feffb4a10def027ab756770b1154214a80524f51
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-25 17:03:59 +0530

    Starting Arrays lecture, completed intro to arrays

---
+++ Arrays/Arrays.js
@@ -0,0 +1,13 @@
+// Array in JS
+// I) Intro to arrays
+/* The Array object, as with arrays in other programming languages, 
+enables storing a collection of multiple items under a single variable name, 
+and has members for performing common array operations. */
+let marks = [98, 12, 3, 50, 34, 67, 81, 42, 27, 75];
+console.log(marks);
+console.log(marks.length);
+
+let bibleCharacters = ["Abraham", "Isaac", "Jacob", "Moses", "Aaron", "Joseph", "David", "Jesus Christ"];
+console.log(bibleCharacters);
+
+// II) Array Indices
