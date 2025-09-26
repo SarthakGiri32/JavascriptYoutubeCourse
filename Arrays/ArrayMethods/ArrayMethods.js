2030b5b01c6a3a0061e679e8a1d3ed18ee16479e
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-26 22:04:02 +0530

    Adding code to understand array methods push(), pop() and toString()

---
+++ Arrays/ArrayMethods/ArrayMethods.js
@@ -0,0 +1,20 @@
+// I) push()
+// let bibleCharacters = ["Abraham", "Isaac", "Jacob", "Moses", "Aaron", "Joseph", "David", "Jesus Christ"];
+// let newBibleCharactersLength = bibleCharacters.push("Mary", "Matthew", "Mark", "Luke", "John");
+// console.log(`The changed array with new length of ${newBibleCharactersLength}:`);
+// console.table(bibleCharacters);
+
+// II) pop()
+// let removedFromEndBibleCharacter = bibleCharacters.pop();
+// console.log(`\nDisplaying the name of the bible character removed from the end of the array: ${removedFromEndBibleCharacter}`);
+// console.log(`\nThe changed array with new length of ${bibleCharacters.length}:`);
+// console.table(bibleCharacters);
+
+// III) toString()
+let marks = [34, 51, 1, 100, 72];
+console.log(`String representation of bible characters array: ${marks.toString()}`);
+console.log(`String representation of bible characters array with Javascript calling\ntoString() automatically without a explicit call: ${marks}`);
+// implicitly called since array is represented as a text value
+
+// IV) concat()
+
