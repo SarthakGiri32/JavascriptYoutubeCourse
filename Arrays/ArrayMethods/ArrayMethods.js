bb9824c63b09c77c40a193517777c315fc9c3840
Sarthak Giri <giri.sarthak1@gmail.com>
2025-09-29 20:42:16 +0530

    Adding code for understanding concat function

--- Arrays/ArrayMethods/ArrayMethods.js
+++ Arrays/ArrayMethods/ArrayMethods.js
@@ -1,6 +1,6 @@
 // I) push()
-// let bibleCharacters = ["Abraham", "Isaac", "Jacob", "Moses", "Aaron", "Joseph", "David", "Jesus Christ"];
-// let newBibleCharactersLength = bibleCharacters.push("Mary", "Matthew", "Mark", "Luke", "John");
+let bibleCharacters = ["Abraham", "Isaac", "Jacob", "Moses", "Aaron", "Joseph", "David", "Jesus Christ"];
+let newBibleCharactersLength = bibleCharacters.push("Mary", "Matthew", "Mark", "Luke", "John");
 // console.log(`The changed array with new length of ${newBibleCharactersLength}:`);
 // console.table(bibleCharacters);
 
@@ -11,10 +11,13 @@
 // console.table(bibleCharacters);
 
 // III) toString()
-let marks = [34, 51, 1, 100, 72];
-console.log(`String representation of bible characters array: ${marks.toString()}`);
-console.log(`String representation of bible characters array with Javascript calling\ntoString() automatically without a explicit call: ${marks}`);
+// let marks = [34, 51, 1, 100, 72];
+// console.log(`String representation of marks array: ${marks.toString()}`);
+// console.log(`String representation of marks array with Javascript calling\ntoString() automatically without a explicit call: ${marks}`);
 // implicitly called since array is represented as a text value
 
 // IV) concat()
-
+let wondersOfTheWorld = ["Taj Mahal", "Great Wall of China", "Christ the Redeemer", "Chichen Itza", "Petra", "Macchu Pichu", "Colosseum", "Calvary"];
+let combinedWondersAndBibleCharacters = wondersOfTheWorld.concat(bibleCharacters, ["James", "Peter", "John", "Paul"]);
+console.log("The combined arrays of wonders of the world and bible characters:");
+console.table(combinedWondersAndBibleCharacters);
